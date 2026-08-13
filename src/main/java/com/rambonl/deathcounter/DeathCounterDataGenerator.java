ddc16eb681bb0e5bde7a067aae7c308ad374605c
RambonL <renderman99@hotmail.de>
2026-08-13 13:57:02 +0200

    Initial commit

---
+++ src/main/java/com/rambonl/deathcounter/DeathCounterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rambonl.deathcounter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DeathCounterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
